ab375d24e1901463f4effcfa5538203ed4df3dc9
VenkatSaiSunil <venkatsaisunil@gmail.com>
2026-03-06 18:41:15 +0530

    updated terraform file with docker installation script

--- AWS Terraform/Project2/Terraform/main.tf
+++ AWS Terraform/Project2/Terraform/main.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "Pipeline_Instance" {
   instance_type          = "t3.micro"
   ami                    = "ami-0f3caa1cf4417e51b"
   key_name               = data.aws_key_pair.existing_key.key_name
+    user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum install docker -y
+systemctl start docker
+systemctl enable docker
+EOF
+  
 
   vpc_security_group_ids = ["sg-03e4f476ac3735dee"]
 
